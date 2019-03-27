f6c9d4c240c884cacf900537e2ac1b56dd7e9c5a
zipit145 <gregbforney@gmail.com>
2019-03-27 16:42:38 -0600

    production port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express') //installed no need for file path
 const app = express()
 const queries = require('./queries')
 
-const port = 3001
+const port = process.env.PORT || 3001
 app.get('/',(req, res, next) => {
     console.log("hi")
     queries.readAllUsers().then(users => res.send(users))
