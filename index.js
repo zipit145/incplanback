d6c18dbcc4f7c2eea7aeaf6c1c5bfc79b0bd88c0
zipit145 <gregbforney@gmail.com>
2019-03-28 15:08:06 -0600

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express') //installed no need for file path
 const app = express()
 const queries = require('./queries')
+const cors = require('cors')
 
 const port = process.env.PORT || 3001
+app.use(cors())
 app.get('/',(req, res, next) => {
     console.log("hi")
     queries.readAllUsers().then(users => res.send(users))
